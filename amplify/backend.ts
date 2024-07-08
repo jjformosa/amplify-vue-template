7e99e216d489511cf40834a8a7fbc8e53431661c
jay_rt <>
2024-07-08 13:38:05 +0800

    fix:use exist cognito configure

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -21,7 +21,7 @@ backend.addOutput({
     'aws_region': 'us-east-1',
     'user_pool_id': 'us-east-1_W2eOnMevj',
     'user_pool_client_id': '48q7ujh2ad84qmdpqvntd485k',
-    'identity_pool_id': 'us-east-1:4b566e85-e5b3-4d13-ae68-0fce25b6bc6-4d13-ae68-0fce25b6bc61',
+    'identity_pool_id': 'us-east-1:14c097d0-ec5e-45a2-b86e-d219d6507964',
     "standard_required_attributes": [
       "email"
     ],
