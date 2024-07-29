65d44f039b601ec81e58af84c6183ffe497a56ee
jay_rt <>
2024-07-29 09:15:30 +0800

    暫時停用migration

--- amplify/migration/240727.ts
+++ amplify/migration/240727.ts
@@ -5,39 +5,39 @@ import type { SchemaAssessmentTopic } from '../data/resource'
 const doSomething = async () => {
   await doSignIn()
 
-  const clientAssessmentTopic = generateClient<SchemaAssessmentTopic>()
+  // const clientAssessmentTopic = generateClient<SchemaAssessmentTopic>()
   
-  const listResposne = (await clientAssessmentTopic.models.Response.list()).data
-  const listTopic = (await clientAssessmentTopic.models.Topic.list()).data
-  let scenarioQuery = null, listScenario = null, scenarioNextToken = null
-  scenarioQuery = await clientAssessmentTopic.models.Scenario.list({
-    limit: 10
-  })
-  listScenario = scenarioQuery.data
-  scenarioNextToken = scenarioQuery.nextToken
-  for (const topicIndex in listTopic) {
-    const _topic = listTopic[topicIndex]
-    for (const scenarioIndex in listScenario) {
-      const _scenario = listScenario[scenarioIndex]
-      const _question = (await clientAssessmentTopic.models.Question.create({
-        topicId: _topic.id
-      })).data
-      if (_question) {
-        await clientAssessmentTopic.models.Scenario.update({
-          id: _scenario.id,
-          questionId: _question.id
-        })
-        for (const responseIndex in listResposne) {
-          const _response = listResposne[responseIndex]
-          await clientAssessmentTopic.models.Response.update({
-            id: _response.id,
-            questionId: _question.id
-          })
-        }
-      }
-    }
-    break
-  }
+  // const listResposne = (await clientAssessmentTopic.models.Response.list()).data
+  // const listTopic = (await clientAssessmentTopic.models.Topic.list()).data
+  // let scenarioQuery = null, listScenario = null, scenarioNextToken = null
+  // scenarioQuery = await clientAssessmentTopic.models.Scenario.list({
+  //   limit: 10
+  // })
+  // listScenario = scenarioQuery.data
+  // scenarioNextToken = scenarioQuery.nextToken
+  // for (const topicIndex in listTopic) {
+  //   const _topic = listTopic[topicIndex]
+  //   for (const scenarioIndex in listScenario) {
+  //     const _scenario = listScenario[scenarioIndex]
+  //     const _question = (await clientAssessmentTopic.models.Question.create({
+  //       topicId: _topic.id
+  //     })).data
+  //     if (_question) {
+  //       await clientAssessmentTopic.models.Scenario.update({
+  //         id: _scenario.id,
+  //         questionId: _question.id
+  //       })
+  //       for (const responseIndex in listResposne) {
+  //         const _response = listResposne[responseIndex]
+  //         await clientAssessmentTopic.models.Response.update({
+  //           id: _response.id,
+  //           questionId: _question.id
+  //         })
+  //       }
+  //     }
+  //   }
+  //   break
+  // }
 }
 
 doSomething()
